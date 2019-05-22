f00763c6a6636151cfdac45533bfcf7aff8ec532
João Santos <joao.mp.santos@ua.pt>
2019-05-22 19:51:36 +0100

    Fix Bug on previous Commit

--- services/sensilotemp/src/main.rs
+++ services/sensilotemp/src/main.rs
@@ -1,6 +1,6 @@
 extern crate actix_web;
 
-use actix_web::{server, HttpRequest}
+use actix_web::{server, HttpRequest, App};
 
 
 
@@ -9,8 +9,8 @@ fn index(_req: &HttpRequest) -> &'static str {
 }
 
 fn main() {
-    server::new(|| App::new().resource("/", |r| r.f(index)))
-        .bind("127.0.0.1:8088")
+    server::new(|| {App::new().resource("/", |r| r.f(index))})
+        .bind("127.0.0.1:8080")
         .unwrap()
         .run();
 }
