521ba4446357d7dd06f8fdf9aaa25318a842f5de
João Santos <joao.mp.santos@ua.pt>
2019-05-22 18:39:09 +0100

    Initial Rust Boilerplate

---
+++ services/rustboiler/src/main.rs
@@ -0,0 +1,15 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+
+#[get("/")]
+fn root() -> &'static str {
+    "Hello World"
+}
+
+fn main() {
+    rocket::ignite()
+    .mount("/", routes![root])
+    .launch();
+}
